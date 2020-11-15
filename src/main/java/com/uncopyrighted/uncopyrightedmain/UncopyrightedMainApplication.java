1030aa9ac9a9e65e6cbe095258c4cd71357e51fa
GoldCastillo <mikko.linna@myy.haaga-helia.fi>
2020-11-15 17:29:56 +0200

    first ccommit

---
+++ src/main/java/com/uncopyrighted/uncopyrightedmain/UncopyrightedMainApplication.java
@@ -0,0 +1,13 @@
+package com.uncopyrighted.uncopyrightedmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncopyrightedMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncopyrightedMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uncopyrighted/uncopyrightedmain/UncopyrightedMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncopyrighted.uncopyrightedmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncopyrightedMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
